a78d549859bba8f7b5bbaa99ecd735f596288349
Park Dong-Ha <luncliff@gmail.com>
2016-11-03 10:19:16 +0000

    ﻿Rewrote Go's Sequential version
    Done
     - [Go] : Sequential code
    
    To Do
     - [Go] : Parallel
     - [C++] : C++ TBB setup
     - [C++] : Sequential/Parallel

--- Go/main.go
+++ Go/main.go
@@ -2,60 +2,93 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
+	"watch"
 )
 
 const (
-	n = 10
+	// This is maximum N
+	N = 1 << 13
 )
 
 var (
-	cost [n + 1][n + 1]float64
-	root [n + 1][n + 1]int
-	prob [n]float64
+	cost [N + 1][N + 1]float64
+	root [N + 1][N + 1]int
+	prob [N]float64
 )
 
-func mst(i, j int) {
-	var bestCost float64 = 1e9 + 0.0
+// Initialize variables
+func init() {
+	// ---- ---- ---- ---- ----
+	var total float64
+	for i := 0; i < N; i++ {
+		var value float64 = rand.Float64() / 10
+		total += value
+		prob[i] = value
+	}
+
+	for i := 0; i < N; i++ {
+		prob[i] = prob[i] / total
+	}
+	// ---- ---- ---- ---- ----
+
+}
+
+func main() {
+	w := watch.StopWatch{}
+	w.Reset()
+
+	init()
+
+	fmt.Printf("[Size] : %d \n", N)
+	w.Reset()
+
+	// Calculation
+	// ---- ---- ---- ---- ----
+	for i := N; i >= 0; i-- {
+		for j := i; j <= N; j++ {
+			root[i][j], cost[i][j] = Sequential(i, j)
+		}
+	}
+	// ---- ---- ---- ---- ----
+
+	dur := w.Reset()
+	sec := dur.Seconds()
+	fmt.Printf("[Time] : %0.3f sec \n", sec)
+	// nano := dur.Nanoseconds()
+	// fmt.Printf("[Time] : %d ns \n", nano)
+
+}
+
+func Sequential(r, c int) (Root int, Cost float64) {
+	var bestCost float64 = math.MaxFloat64
 	var bestRoot int = -1
 
 	switch {
-	case i >= j:
-		cost[i][j] = 0.0
-		root[i][j] = -1
-	case i+1 == j:
-		cost[i][j] = prob[i]
-		root[i][j] = i + 1
-	case i+1 < j:
-		psum := 0.0
-		for k := i; k <= j-1; k++ {
-			psum += prob[k]
+	case r >= c: // Unused range
+		Root, Cost = -1, 0.0
+
+	case r+1 == c: // Main diagonal
+		Root, Cost = r+1, prob[r]
+
+	case r+1 < c: // BST
+
+		sum := 0.0 // basic cost
+		for k := r; k < c; k++ {
+			sum += prob[k]
 		}
-		for r := i; r <= j-1; r++ {
-			rcost := cost[i][r] + cost[r+1][j]
-			if rcost < bestCost {
-				bestCost = rcost
-				bestRoot = r + 1
+
+		// find optimized case
+		for i := r; i < c; i++ {
+			rCost := cost[r][i] + cost[i+1][c]
+			if rCost < bestCost {
+				bestCost = rCost
+				bestRoot = i + 1
 			}
-			cost[i][j] = bestCost + psum
-			root[i][j] = bestRoot
 		}
+		Root, Cost = bestRoot, bestCost+sum
 	} //switch
+	return
 
 } // mst
-
-func main() {
-	// initialize prob[]
-	for i := n; i >= 0; i-- {
-		for j := i; j <= n; j++ {
-			mst(i, j)
-		}
-	}
-	for i := n; i >= 0; i-- {
-		for j := i; j <= n; j++ {
-			fmt.Println(root[i][j])
-		}
-	}
-
-	fmt.Println("Hello, Go!")
-
-}
---
+++ Go/src/watch/watch.go
@@ -0,0 +1,20 @@
+﻿package watch
+
+import (
+	"time"
+)
+
+type StopWatch struct {
+	time.Time
+}
+
+func (watch *StopWatch) Pick() time.Duration {
+	now := time.Now()
+	return now.Sub(watch.Time)
+}
+
+func (watch *StopWatch) Reset() time.Duration {
+	span := watch.Pick()
+	watch.Time = time.Now()
+	return span
+}
