6e80991e2f640de645dbd8a6b03fcbb21da3ee60
Park Dong-Ha <luncliff@gmail.com>
2016-12-05 07:44:39 +0000

    Added matrix.go

---
+++ Go/src/matrix/matrix.go
@@ -0,0 +1,42 @@
+﻿package matrix
+
+// Int2D ...
+// 		Allocate large `int` array
+//      - Note
+//          overflow handling required
+func Int2D(row int, col int) (mat [][]int) {
+	size := row * col
+	arr := make([]int, size)
+
+	for i := 0; i < size; i += col {
+		part := arr[i : i+col]
+		mat = append(mat, part)
+	}
+	return
+}
+
+// Float642D ...
+// 		Allocate large `float64 array
+func Float642D(row int, col int) (mat [][]float64) {
+	size := row * col
+	arr := make([]float64, size)
+
+	for i := 0; i < size; i += col {
+		part := arr[i : i+col]
+		mat = append(mat, part)
+	}
+	return
+}
+
+// ChanInt2D ...
+// 		Allocate large `chan int`
+func ChanInt2D(row int, col int) (mat [][]chan int) {
+	size := row * col
+	arr := make([]chan int, size)
+
+	for i := 0; i < size; i += col {
+		part := arr[i : i+col]
+		mat = append(mat, part)
+	}
+	return
+}
